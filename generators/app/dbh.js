d553449543311056cd855305a5e57d89a2bf70cf
Bastien Michaux <bastienmichaux@gmail.com>
2017-03-27 12:07:22 +0200

    Fix typo (but who cares amirite?)

--- generators/app/dbh.js
+++ generators/app/dbh.js
@@ -3,13 +3,15 @@
 const chalk = require('chalk');
 
 module.exports = {
+
+
     // use this as a DEBUG logger
     _dbhDebugLog : function (pString) {
         if (typeof pString === "string" && pString !== '') {
             console.log(chalk.bold.green("DEBUG : " + pString));
         } else {
-			// log obvious, shameful mistake
-            console.log(chalk.bold.red("_dbhDebugLog : bad parameter ! "));
+            // log obvious, shameful mistake
+            console.log(chalk.bold.red("_dbhDebugLog : bad parameter !"));
             console.log(chalk.red("pString : type = " + typeof pString + ", value = " + pString));
         }
     }
