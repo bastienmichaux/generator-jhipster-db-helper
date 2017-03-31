8810012a88c2dc8523a0d139d39a609ce72a51ae
Bastien Michaux <bastienmichaux@gmail.com>
2017-03-31 11:30:05 +0200

    Untrack previously ignored files

--- generators/app/db-helper.js
+++ generators/app/db-helper.js
@@ -5,19 +5,22 @@
  * - write proper JsDoc
  * - create an ensemble unit test
  * - replace 'console.log' with 'this.log'
+ * - maybe replace the NPM module called 'replace' (I know, it's confusing) with lodash.replace function (but beware, this functions uses the arguments property).
  * Don't forget to run eslint !
  */
 
 const chalk = require('chalk');
 const replace = require('replace');
 const fs = require('fs');
-const generator = require('yeoman-generator');
+const Generator = require('yeoman-generator');
 
+
+console.log("yo dawg");
 
 /**
  * Configuration files in generator-jhipster that include the Spring naming strategies (as of JHipster 4.1.1).
  * These files are replaced by our module to avoid inconsistencies when mapping over an existing DB.
- * This constant assumes the files contain the naming strategies and isn't future-proof. Maybe we should consider a dynamic initialisation instead of a static one.
+ * This constant could be dynamically initialized instead of being static. It isn't future-proof.
  * @constant
  * @todo Add relevant links (StackOverflow) to this doc
  * @type {string[]}
@@ -67,29 +70,44 @@ const physicalNamingStrategyNew = 'org.hibernate.boot.model.naming.PhysicalNamin
 /** return true for a non-empty string */
 const isTrueString = x => (typeof x === 'string' && x !== '');
 
-module.exports = {
+
+module.exports = class extends Generator {
+    x () {
+        this.log ("Hooray!!!!");
+    }
 
     /** We use this function as a DEBUG logger during development. Users shouldn't see it. */
     debugLog(pString) {
         if (isTrueString(pString)) {
-            console.log(chalk.bold.green(`DBH-DEBUG: ${pString}`));
+            console.log(chalk.bold.yellow(`DBH-DEBUG: ${pString}`));
         } else {
-            // log obvious, shameful mistake
-            console.log(chalk.bold.red('debugLog : bad parameter !'));
-            console.log(chalk.red(`pString : type = ${typeof pString}, value = ${pString}`));
+            throw new TypeError(
+                chalk.red(`DBH: pString isn't a true string: type = ${typeof pString} ; value = ${pString}`)
+            );
         }
-    },
+    }
 
     /** We use this function to warn the user. */
     warnLog(pString) {
         if (isTrueString) {
             console.log(chalk.bold.red(`DBH-WARN: ${pString}`));
         } else {
-            // log obvious, shameful mistake
-            console.log(chalk.bold.red('warnLog : bad parameter !'));
-            console.log(chalk.red(`pString : type = ${typeof pString}, value = ${pString}`));
+            throw new TypeError(
+                chalk.red(`DBH: pString isn't a true string: type = ${typeof pString} ; value = ${pString}`)
+            );
         }
-    },
+    }
+
+    /** Hooray ! Celebrate something. */
+    successLog(pString) {
+        if (isTrueString) {
+            console.log(chalk.bold.green(`DBH-SUCCESS: ${pString}`));
+        } else {
+            throw new TypeError(
+                chalk.red(`DBH: pString isn't a true string: type = ${typeof pString} ; value = ${pString}`)
+            );
+        }
+    }
 
     /**
      * Test if Spring naming strategies are replaced by our naming strategies
@@ -99,19 +117,24 @@ module.exports = {
     namingStrategiesReplaced() {
         console.log(chalk.bold.red('getEntityNameVariations NOT IMPLEMENTED YET !'));
         return false;
-    },
+    }
 
     /**
-     * Return an object with the entity name and all its variants (name, tableName, entityTableName, etc)
+     * Return an object with the entity name and all its variants (name, tableName, entityTableName, etc).
      * @todo Write unit test
      * @returns {Object}
      */
     getEntityNameVariations(pEntityName) {
         console.log(chalk.bold.red('getEntityNameVariations NOT IMPLEMENTED YET !'));
-        return false;
-    },
+        if (isTrueString(pEntityName)) {
+            return false;
+        } else {
+            throw new TypeError(`pEntityName isn't a true string: type = ${typeof pEntityName} ; value = ${pEntityName}`);
+        }
+    }
 
-    /** replace Spring naming strategies with more neutral ones
+    /**
+     * replace Spring naming strategies with more neutral ones
      * return true if all occurrences are replaced
      *
      * note : after running this function, reference to the ancient naming strategies will still be found in :
@@ -164,3 +187,5 @@ module.exports = {
         return false;
     }
 };
+
+
