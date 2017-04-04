014077de9f7b0940723451d06aadb0cd6c6eb516
Adrien Horgnies <aHorgnies@altissia.com>
2017-04-04 10:34:21 +0200

    post entity hook now replace table name for config and ORM

--- generators/fix-entity/index.js
+++ generators/fix-entity/index.js
@@ -2,6 +2,7 @@ const generator = require('yeoman-generator');
 const chalk = require('chalk');
 
 const replace = require('replace');
+const fs = require('fs');
 
 const jhipsterVar = {
     moduleName: 'fix-entity '
@@ -21,11 +22,32 @@ module.exports = generator.extend({
         }
     },
     doThings() {
-        this.log('Whole entity file');
-        this.log(this.options.entityConfig);
+        // this.log(chalk.blue('Whole entity file'));
+        // this.log(this.options.entityConfig);
+        // this.log(chalk.blue('jhipsterVar'));
+        // this.log(jhipsterVar);
 
-        // As of now, I'll use EntityClass as a default entityTableName. This value should be user input.
+        // TODO As of now, I'll use EntityClass as a default entityTableName. This value should be user input.
         let wantedValue = this.options.entityConfig.entityClass;
+        let unwantedValue = this.options.entityConfig.entityTableName;
+
+        let ORMFile = jhipsterVar.javaDir + '/domain/' + this.options.entityConfig.entityClass + '.java';
+
+        if (fs.existsSync(ORMFile)) {
+            this.log(`File ${chalk.cyan(ORMFile)} exists`);
+            let prefix = '@Table\\(name = "';
+            let suffix = '"\\)';
+
+            replace({
+                regex: prefix + '.*' + suffix,
+                replacement: prefix.replace('\\', '') + wantedValue + suffix.replace('\\', ''),
+                paths: [ORMFile],
+                recursive: false,
+                silent: true,
+            });
+        } else {
+            throw new Error(`${ORMFile} doesn't exist!`);
+        }
 
         jhipsterFunc.updateEntityConfig(this.options.entityConfig.filename, 'entityTableName', wantedValue);
     },
