26e7f73a881475390d2ea2402bf909143a771f9b
Bastien Michaux <bastienmichaux@gmail.com>
2017-04-04 09:17:26 +0200

    Change folder organization for test files

---
+++ test/fix-entity/index.js
@@ -0,0 +1,38 @@
+/* global describe, beforeEach, it*/
+
+const path = require('path');
+const fse = require('fs-extra');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+
+const deps = [
+    [helpers.createDummyGenerator(), 'jhipster:modules']
+];
+
+describe('JHipster generator db-helper', () => {
+    describe('simple test', () => {
+        beforeEach((done) => {
+            helpers
+                .run(path.join(__dirname, '../generators/app'))
+                .inTmpDir((dir) => {
+                    fse.copySync(path.join(__dirname, '../test/templates/default'), dir);
+                })
+                .withOptions({
+                    testmode: true
+                })
+                .withPrompts({
+                    message: 'simple message to say hello'
+                })
+                .withGenerators(deps)
+                .on('end', done);
+        });
+        /*
+        it('find no occurrences of org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy', () => {
+            assert.fileContent(file, /org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy/);
+        });
+        it('find no occurrences of org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy', () => {
+            assert.fileContent(file, /org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy/);
+        });
+        */
+    });
+});
