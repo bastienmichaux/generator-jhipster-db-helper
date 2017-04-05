dc37993fbe04e6d4c16408189a4daa4266f34d41
Bastien Michaux <bastienmichaux@gmail.com>
2017-04-05 10:18:24 +0200

    Add doc

--- generators/fix-entity/index.js
+++ generators/fix-entity/index.js
@@ -1,16 +1,18 @@
-// modules used by the generator
 const generator = require('yeoman-generator');
 const assert = require('yeoman-assert');
 const chalk = require('chalk');
 const prompts = require('./prompts.js');
-
 const replace = require('replace');
 const fs = require('fs');
+
 
 const jhipsterVar = {
     moduleName: 'fix-entity '
 };
+
+
 const jhipsterFunc = {};
+
 
 module.exports = generator.extend({
     constructor: function (...args) { // eslint-disable-line object-shorthand
@@ -19,6 +21,8 @@ module.exports = generator.extend({
         this.defaultTableName = this.options.entityConfig.entityClass;
         this.tableNameInput;
     },
+
+
     // check current project state, get configs, etc
     initializing() {
         this.log('fix-entity generator');
@@ -29,11 +33,13 @@ module.exports = generator.extend({
 		);
     },
 
+
     // prompt the user for options
     prompting: {
         askForTableName: prompts.askForTableName,
         askForColumnName: prompts.askForColumnName
     },
+
 
     // other Yeoman run loop steps would go here :
 
@@ -41,16 +47,30 @@ module.exports = generator.extend({
 
     // default() : If the method name doesn't match a priority, it will be pushed to this group.
 
-    // write the generator-specific files
+
+    /**
+     * After creating a new entity, replace the value of the table name.
+     *
+     * Allows consistent mapping with an existing database table without modifying JHipster's entity subgenerator.
+     **/
     writing() {
         // DEBUG : log where we are
         this.log('writing');
 
+        // path of the Java entity class
+        // something like : src/main/java/package/domain/Foo.java
         let ORMFile = jhipsterVar.javaDir + '/domain/' + this.entityConfig.entityClass + '.java';
-        let liquiFile = jhipsterVar.resourceDir + 'config/liquibase/changelog/' + this.entityConfig.data.changelogDate + '_added_entity_' + this.entityConfig.entityClass + '.xml';
+
+        // path of the Liquibase changelog file
+        // something like : src/main/resources/config/liquibase/changelog/20150128232313_added_entity_Foo.xml
+        let liquibaseFile = jhipsterVar.resourceDir + 'config/liquibase/changelog/' + this.entityConfig.data.changelogDate + '_added_entity_' + this.entityConfig.entityClass + '.xml';
+
+        // wanted table name (replaces JHipster's automatically created table name)
         let desiredTableName = this.tableNameInput;
 
-        assert.file([ORMFile, liquiFile]);
+        assert.file([ORMFile, liquibaseFile]);
+
+        // replace the value of the 'name' attribute for @Table in the Java entity class
 
         let prefix = '@Table\\(name = "';
         let suffix = '"\\)';
@@ -63,27 +83,33 @@ module.exports = generator.extend({
             silent: true,
         });
 
+        // replace the value of the 'tableName' attribute in the liquibase _added_entity file for this entity
+
         prefix = '<createTable tableName="';
         suffix = '">';
 
         replace({
             regex: prefix + '.*' + suffix,
             replacement: prefix + desiredTableName + suffix,
-            paths: [liquiFile],
+            paths: [liquibaseFile],
             recursive: false,
             silent: true,
         });
 
+        // update the entity json file
         jhipsterFunc.updateEntityConfig(this.entityConfig.filename, 'entityTableName', desiredTableName);
     },
 
+
     // conflict() : Where conflicts are handled (used internally)
+
 
     // run installation (npm, bower, etc)
     install() {
         // DEBUG : log where we are
         this.log('install');
     },
+
 
     // cleanup, say goodbye
     end() {
