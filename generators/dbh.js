cf75e0d35d0a7b87b2f896850d4b5618f53da980
Bastien Michaux <bastienmichaux@gmail.com>
2017-04-13 12:19:35 +0200

    Remove useless check

--- generators/dbh.js
+++ generators/dbh.js
@@ -4,9 +4,6 @@ const fs = require('fs');
 /** return the content of .yo-rc.json as a JSON object */
 const getAppConfig = directory => new Promise((resolve, reject) => {
     const path = directory + DBH_CONSTANTS.appConfigFile;
-    if (!isTrueString(path)) {
-        reject(new Error(`getAppConfig: bad parameter: ${typeof path} ${path})`));
-    }
     // if file exists, return its output as a JSON object
     if (fs.existsSync(path)) {
         fs.readFile(path, 'utf8', (err, data) => {
