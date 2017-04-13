30ef6abb32c90cc17a3bf558ea84bcc73037e32d
Bastien Michaux <bastienmichaux@gmail.com>
2017-04-13 11:06:20 +0200

    Refactor promise

--- generators/dbh.js
+++ generators/dbh.js
@@ -1,28 +1,27 @@
 const DBH_CONSTANTS = require('./dbh-constants');
 const fs = require('fs');
 
-
 /** return the content of .yo-rc.json as a JSON object */
 const getAppConfig = directory => new Promise((resolve, reject) => {
     const path = directory + DBH_CONSTANTS.appConfigFile;
     if (!isTrueString(path)) {
-        throw new Error(`getAppConfig: bad parameter: ${typeof path} ${path})`);
+        reject(new Error(`getAppConfig: bad parameter: ${typeof path} ${path})`));
     }
     // if file exists, return its output as a JSON object
     if (fs.existsSync(path)) {
         fs.readFile(path, 'utf8', (err, data) => {
             if (err) {
-                throw new Error(err);
+                reject(new Error(err));
             }
             const appConfigToJson = JSON.parse(data);
             if (appConfigToJson) {
                 resolve(appConfigToJson);
             } else {
-                reject(`getAppConfig: no output. Type: ${typeof appConfigToJson}, value: ${appConfigToJson}`);
+                reject(new Error(`getAppConfig: no output. Type: ${typeof appConfigToJson}, value: ${appConfigToJson}`));
             }
         });
     } else {
-        throw new Error(`getAppConfig: file ${path} not found`);
+        reject(new Error(`getAppConfig: file ${path} not found`));
     }
 });
 
