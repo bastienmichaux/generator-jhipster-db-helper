6e4b68718bcb5ee74ea9c6eed26814450e17db0a
Bastien Michaux <bastienmichaux@gmail.com>
2017-04-14 10:57:16 +0200

    Correct validateTableName

--- generators/dbh.js
+++ generators/dbh.js
@@ -1,5 +1,6 @@
 const DBH_CONSTANTS = require('./dbh-constants');
 const fs = require('fs');
+const jhipsterCore = require('jhipster-core');
 
 
 /** a promise returning the current JHipster app config as a JSON object */
@@ -45,16 +46,22 @@ const validateColumnName = (input, dbType) => {
 };
 
 
-/** validate user input when asking for a SQL table name */
+/**
+ * Validate user input when asking for a SQL table name
+ * This function closely follows JHipster's own validateTableName function
+ * (in generator-jhipster/generators/entity/index.js)
+ **/
 const validateTableName = (input, dbType) => {
     if (!/^([a-zA-Z0-9_]*)$/.test(input)) {
         return 'The table name cannot contain special characters';
     } else if (input === '') {
         return 'The table name cannot be empty';
-    } else if (dbType === 'oracle' && input.length > 14) {
+    } else if (dbType === 'oracle' && input.length > 26) {
         return 'The table name is too long for Oracle, try a shorter name';
-    } else if (input.length > 30) {
-        return 'The table name is too long, try a shorter name';
+    } else if (dbType === 'oracle' && input.length > 14) {
+        return 'The table name is long for Oracle, long table names can cause issues when used to create constraint names and join table names';
+    } else if (jhipsterCore.isReservedTableName(input, dbType)) {
+        return `'${input}' is a ${dbType} reserved keyword.`;
     }
     return true;
 };
