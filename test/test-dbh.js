66a377d41b3ef3907d9a5372bc2aae425b0bec70
Bastien Michaux <bastienmichaux@gmail.com>
2017-04-24 18:29:10 +0200

    more unit tests

--- test/test-dbh.js
+++ test/test-dbh.js
@@ -2,8 +2,10 @@ const path = require('path');
 const fse = require('fs-extra');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
+const _ = require('lodash');
 
 const dbh = require('../generators/dbh.js');
+const DBH_CONSTANTS = require('../generators/dbh-constants');
 
 const expectedAppConfig = {
     "generator-jhipster": {
@@ -33,19 +35,38 @@ const expectedAppConfig = {
 // Dbh unit test
 describe('Dbh', function () {
     describe('getColumnIdName', function () {
-        it('works as expected with treacherous inputs', function () {
+        it('works as expected with treacherous input', function () {
             assert(dbh.getColumnIdName('Authors') === 'authors_id');
             assert(dbh.getColumnIdName('AUTHORS') === 'authors_id');
             assert(dbh.getColumnIdName('IT_IS_OVER_9000') === 'it_is_over_9000_id');
             assert(dbh.getColumnIdName('AuthorTable') === 'author_table_id');
-            assert(dbh.getColumnIdName('TheAuthor_table' === 'the_author_table_id'));
+            assert(dbh.getColumnIdName('TheAuthor_table') === 'the_author_table_id');
             assert(dbh.getColumnIdName('XMLHTTPPosts') === 'xmlhttpposts_id');
-            assert(dbh.getColumnIdName('CssService' === 'css_service_id'));
-            assert(dbh.getColumnIdName('CSSService' === 'cssservice_id'));
+            assert(dbh.getColumnIdName('CssClassesService') === 'css_classes_service_id');
+            assert(dbh.getColumnIdName('CSSClassesService') === 'cssclasses_service_id');
         });
     });
     describe('getFilesWithNamingStrategy', function () {
-        it('');
+        it(`excludes the Gradle file(s) with 'maven' as parameter`, function () {
+            const files = dbh.getFilesWithNamingStrategy('maven');
+            assert(_.isEqual(files, [
+                './pom.xml',
+                './src/main/resources/config/application.yml',
+                './src/test/resources/config/application.yml'
+            ]));
+        });
+        it(`excludes the Maven file(s) with 'gradle' as parameter`, function () {
+            const files = dbh.getFilesWithNamingStrategy('gradle');
+            assert(_.isEqual(files, [
+                './src/main/resources/config/application.yml',
+                './src/test/resources/config/application.yml',
+                './gradle/liquibase.gradle',
+            ]));
+        });
+        it(`fails with an unknown parameter`, function () {
+            let foo;
+            //assert.throws(() => foo = dbh.getFilesWithNamingStrategy('foo'), Error);
+        });
     });
     describe('getPluralColumnIdName', function () {
         it('');
@@ -57,13 +78,43 @@ describe('Dbh', function () {
         it('');
     });
     describe('validateColumnName', function () {
-        it('');
+        // messages output by validateColumnName
+        // TODO: 0% maintainability, find something smarter
+        const failMsgWhenSpecialChar = 'Your column name cannot contain special characters';
+        const failMsgWhenEmpty = 'Your column name cannot be empty';
+        const failMsgWhenTooLongForOracle = 'Your column name is too long for Oracle, try a shorter name';
+
+        it('works as expected', function () {
+            assert(dbh.validateColumnName('Book', 'mysql') === true);
+            assert(dbh.validateColumnName('FOO', 'mysql') === true);
+            assert(dbh.validateColumnName('bar', 'mysql') === true);
+            assert(dbh.validateColumnName('_foo2', 'mysql') === true);
+            assert(dbh.validateColumnName('2Foo2Bar', 'mysql') === true);
+            assert(dbh.validateColumnName('06', 'mysql') === true);
+            assert(dbh.validateColumnName('_', 'mysql') === true);
+            assert(dbh.validateColumnName('quiteLongTableName', 'mysql') === true);
+            assert(dbh.validateColumnName('definitelyVeryLongTableName', 'mysql') === true);
+        });
+        it(`returns '${failMsgWhenSpecialChar}'`, function () {
+            assert(dbh.validateColumnName(' ', 'mysql') === failMsgWhenSpecialChar);
+            assert(dbh.validateColumnName('Böök', 'mysql') === failMsgWhenSpecialChar);
+            assert(dbh.validateColumnName('book-table', 'mysql') === failMsgWhenSpecialChar);
+        });
+        it(`returns '${failMsgWhenEmpty}'`, function () {
+            assert(dbh.validateColumnName('', 'mysql') === failMsgWhenEmpty);
+        });
+        it(`returns '${failMsgWhenTooLongForOracle}'`, function () {
+            assert(dbh.validateColumnName('definitelyVeryLongTableName', 'oracle') === failMsgWhenTooLongForOracle);
+        });
     });
     describe('validateTableName', function () {
+        // messages output by validateTableName
+        // TODO: 0% maintainability, find something smarter
         const failMsgWhenSpecialChar = 'The table name cannot contain special characters';
         const failMsgWhenEmpty = 'The table name cannot be empty';
         const failMsgWhenTooLongForOracle = 'The table name is too long for Oracle, try a shorter name';
         const failMsgWhenLongForOracle = 'The table name is long for Oracle, long table names can cause issues when used to create constraint names and join table names';
+
         it('works as expected', function () {
             assert(dbh.validateTableName('Book', 'mysql') === true);
             assert(dbh.validateTableName('FOO', 'mysql') === true);
@@ -91,15 +142,11 @@ describe('Dbh', function () {
         it(`returns '${failMsgWhenLongForOracle}'`, function () {
             assert(dbh.validateTableName('quiteLongTableName', 'oracle') === failMsgWhenLongForOracle);
         });
-        /*
-        it(`returns 'This is a table name`, function () {
-
-        });
-        */
     });
     /*
     describe('', function () {
-        it('');
+        it('', function () {
+        });
     });
     */
 });
