446d8cfaf4c9cf76dc7156bed7d2c4f3481a5a08
Bastien Michaux <bastienmichaux@gmail.com>
2017-05-02 16:06:01 +0200

    cleanup bad testing config

--- test/test-app.js
+++ test/test-app.js
@@ -12,67 +12,19 @@ const deps = [
     [helpers.createDummyGenerator(), 'jhipster:modules']
 ];
 
-
-describe('JHipster generator db-helper', function () {
-    describe('post entity hook', function () {
-        beforeEach(function (done) {
-            helpers
-            .run(path.join(__dirname, '../generators/app/index.js'))
-            .inTmpDir((dir) => {
-                fse.copySync(path.join(__dirname, '../test/templates/default'), dir);
-            })
-            .withOptions({
-                testmode: true,
-                skipInstall: true
-            })
-            .withGenerators(deps)
-            .on('end', done);
-        });
-
-        // test the naming strategies replacement
-
-        it('finds the spring naming strategies before the post app hook');
-
-        it('finds the spring naming strategies replaced after the post hook');
-    });
-});
-
 describe('JHipster-db-helper post app module', () => {
     describe('using Maven', () => {
-        beforeEach((done) => {
-            helpers.run(require.resolve('../generators/app/index.js'))
-            .inTmpDir((dir) => {
-                fse.copySync(path.join(__dirname, '../test/templates/app'), dir);
-            })/*
-            .withArguments([]) // dunnno lol
-            .withPrompts({
-                // x : y // dunno too ;_;
-            })*/
-            .on('end', done);
-        });
-
-        it('replace the naming strategies when the app uses Maven)', () => {
-            //assert.noFile(); // gradle file shouldnt be found
-            //assert.content(); // no reference to the ancient naming strategies, find new strategies instead
+        it('replaces the naming strategies', () => {
+            // gradle file shouldn't be found
+            // no reference to the ancient naming strategies, find new strategies instead
         });
     });
 
-    describe('using Gradle', () => {
-        beforeEach((done) => {
-            helpers.run(require.resolve('../generators/app')) // will probably fail
-            .inTmpDir((dir) => {
-                fse.copySync(path.join(__dirname, '../test/templates/app'), dir); // which folder ?
-            })/*
-            .withArguments([]) // dunnno lol
-            .withPrompts({
-                // x : y // dunno too ;_;
-            })*/
-            .on('end', done);
-        });
 
-        it('replace the naming strategies when the app uses Gradle', () => {
-            //assert.noFile(); // maven file shouldnt be found
-            //assert.content(); // no reference to the ancient naming strategies, find new strategies instead
+    describe('using Gradle', () => {
+        it('replaces the naming strategies', () => {
+            // maven file shouldn't be found
+            // no reference to the ancient naming strategies, find new strategies instead
         });
     });
 });
