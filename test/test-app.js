92e4e2260aebf334d3d9dfd09c3fc0844fd060cf
Bastien Michaux <bastienmichaux@gmail.com>
2017-05-15 10:20:59 +0200

    add pending tests

--- test/test-app.js
+++ test/test-app.js
@@ -6,43 +6,36 @@ const fse = require('fs-extra');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-const dbhConstants = require('../generators/dbh-constants');
+const DBH_CONSTANTS = require('../generators/dbh-constants');
+const DBH_TEST_CONSTANTS = require('../generators/dbh-test-constants');
+
 const Generator = require('../generators/app/index.js');
-const jhim = require('../node_modules/generator-jhipster/generators/modules/index.js');
-const jhib = require('../node_modules/generator-jhipster/generators/generator-base.js');
+const GeneratorBase = require('../node_modules/generator-jhipster/generators/generator-base.js');
 
 const deps = [
     [helpers.createDummyGenerator(), 'jhipster:modules']
 ];
 
-const f = path.join(__dirname, 'templates/default/testReplaceContent.txt');
-jhib.prototype.log = (msg) => {
-    console.log(msg);
-};
+// replace 'this.log' for testing purposes
+GeneratorBase.prototype.log = msg => console.log(msg);
 
 describe('Post app hook', function () {
-   describe('dummy test', function () {
-       it('says foo', function () {
-           assert.textEqual(Generator.prototype._sayFoo(), 'foo');
-       });
-   });
-   describe('polyfill', function () {
-       it('works as expected', function () {
-           console.log(Generator.prototype._polyfillInfo());
-       });
-   });
-   /*
-    describe('using Maven', () => {
-        it('replaces the naming strategies'
-            // gradle file shouldn't be found
-            // no reference to the ancient naming strategies, find new strategies instead
-        );
+    describe('_getPolyfill', function () {
+        beforeEach(function () {
+            return helpers.run(path.join(__dirname + '/../generators/app/index.js'))
+            .inTmpDir(function (dir) {
+                return fse.copySync(path.join(__dirname + '/templates/default/usingMaven'), dir);
+            })
+            .withGenerators(deps)
+            .then(onFulfilled => (onFulfilled), onRejected => console.error(onRejected));
+        });
+        // @todo : have a template config file for testing, file must have : Angular app name, clientFramework, packageManager, etc.
+        it('gives the expected polyfill');
     });
-    describe('using Gradle', () => {
-        it('replaces the naming strategies'
-            // maven file shouldn't be found
-            // no reference to the ancient naming strategies, find new strategies instead
-        );
+    describe('_replaceNamingStrategies (using Gradle)', function () {
+        it('replaces the naming strategies');
     });
-    */
+    describe('_replaceNamingStrategies (using Maven)', function () {
+        it('replaces the naming strategies');
+    });
 });
