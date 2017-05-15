cb4d80e387e2c393fc04691cb564ee2ef3ee2226
Bastien Michaux <bastienmichaux@gmail.com>
2017-05-15 12:37:42 +0200

    add relationships samples to test constants

--- generators/dbh-test-constants.js
+++ generators/dbh-test-constants.js
@@ -1,10 +1,140 @@
 /** constants used by tests */
 
 module.exports = {
+    relationshipsSamples: {
+        oneToOneOwner: [
+            {
+                relationshipType: 'one-to-one',
+                relationshipName: 'region',
+                otherEntityName: 'region',
+                otherEntityField: 'id',
+                ownerSide: true,
+                otherEntityRelationshipName: 'country'
+            }
+        ],
+        mixedWithOneConstraint: [
+            {
+                relationshipType: 'one-to-one',
+                relationshipName: 'location',
+                otherEntityName: 'location',
+                otherEntityField: 'id',
+                ownerSide: true,
+                otherEntityRelationshipName: 'department'
+            },
+            {
+                relationshipType: 'one-to-many',
+                javadoc: 'A relationship',
+                relationshipName: 'employee',
+                otherEntityName: 'employee',
+                otherEntityRelationshipName: 'department'
+            }
+        ],
+        mixedWithTwoConstraints: [
+            {
+                relationshipName: 'department',
+                otherEntityName: 'department',
+                relationshipType: 'many-to-one',
+                otherEntityField: 'id'
+            },
+            {
+                relationshipType: 'one-to-many',
+                relationshipName: 'job',
+                otherEntityName: 'job',
+                otherEntityRelationshipName: 'employee'
+            },
+            {
+                relationshipType: 'many-to-one',
+                relationshipName: 'manager',
+                otherEntityName: 'employee',
+                otherEntityField: 'id'
+            }
+        ],
+        tripleOneToOneOwner: [
+            {
+                relationshipType: 'one-to-one',
+                relationshipName: 'job',
+                otherEntityName: 'job',
+                otherEntityField: 'id',
+                ownerSide: true,
+                otherEntityRelationshipName: 'jobHistory'
+            },
+            {
+                relationshipType: 'one-to-one',
+                relationshipName: 'department',
+                otherEntityName: 'department',
+                otherEntityField: 'id',
+                ownerSide: true,
+                otherEntityRelationshipName: 'jobHistory'
+            },
+            {
+                relationshipType: 'one-to-one',
+                relationshipName: 'employee',
+                otherEntityName: 'employee',
+                otherEntityField: 'id',
+                ownerSide: true,
+                otherEntityRelationshipName: 'jobHistory'
+            }
+        ],
+        mixedConstraints: [
+            {
+                relationshipName: 'employee',
+                otherEntityName: 'employee',
+                relationshipType: 'many-to-one',
+                otherEntityField: 'id'
+            },
+            {
+                relationshipType: 'many-to-many',
+                otherEntityRelationshipName: 'job',
+                relationshipName: 'task',
+                otherEntityName: 'task',
+                otherEntityField: 'title',
+                ownerSide: true
+            }
+        ],
+        Empty: [],
+        manyToManyNotOwner: [
+            {
+                relationshipType: 'many-to-many',
+                relationshipName: 'job',
+                otherEntityName: 'job',
+                ownerSide: false,
+                otherEntityRelationshipName: 'task'
+            }
+        ],
+        oneToOneNotOwner: [
+            {
+                relationshipType: 'one-to-one',
+                relationshipName: 'job',
+                otherEntityName: 'job',
+                ownerSide: false,
+                otherEntityRelationshipName: 'task'
+            }
+        ],
+        mixedNotOwner: [
+            {
+                relationshipType: 'one-to-one',
+                relationshipName: 'job',
+                otherEntityName: 'job',
+                ownerSide: false,
+                otherEntityRelationshipName: 'task'
+            },
+            {
+                relationshipType: 'many-to-many',
+                relationshipName: 'job',
+                otherEntityName: 'job',
+                ownerSide: false,
+                otherEntityRelationshipName: 'task'
+            }
+        ]
+    },
+
+    /*
+     * these objects must be STRICTLY equal to the files
+     * 'templates/default/usingGradle/.yo-rc.json'
+     * 'templates/default/usingMaven/.yo-rc.json'
+     * otherwise tests fail despite a correct implementation
+     */
     templateConfigFile: {
-        // this object must EXACTLY be the object representation of the files
-        // 'templates/default/usingMaven/.yo-rc.json'
-        // otherwise tests fail
         usingGradle: {
             "generator-jhipster": {
                 "baseName": "sampleMysql",
@@ -12,12 +142,12 @@ module.exports = {
                 "packageFolder": "com/mycompany/myapp",
                 "authenticationType": "session",
                 "hibernateCache": "ehcache",
-                "clusteredHttpSession": "no",
-                "websocket": "no",
+                "clusteredHttpSession": false,
+                "websocket": false,
                 "databaseType": "sql",
                 "devDatabaseType": "h2Disk",
                 "prodDatabaseType": "mysql",
-                "searchEngine": "no",
+                "searchEngine": false,
                 "useSass": false,
                 "buildTool": "gradle",
                 "frontendBuilder": "grunt",
@@ -26,7 +156,16 @@ module.exports = {
                 "rememberMeKey": "2bb60a80889aa6e6767e9ccd8714982681152aa5",
                 "testFrameworks": [
                     "gatling"
-                ]
+                ],
+                "jhipsterVersion": "4.3.0",
+                "serverPort": "8080",
+                "messageBroker": false,
+                "serviceDiscoveryType": false,
+                "clientFramework": "angular1",
+                "clientPackageManager": "yarn",
+                "applicationType": "monolith",
+                "jhiPrefix": "jhi",
+                "languages": []
             }
         },
         usingMaven: {
@@ -36,12 +175,12 @@ module.exports = {
                 "packageFolder": "com/mycompany/myapp",
                 "authenticationType": "session",
                 "hibernateCache": "ehcache",
-                "clusteredHttpSession": "no",
-                "websocket": "no",
+                "clusteredHttpSession": false,
+                "websocket": false,
                 "databaseType": "sql",
                 "devDatabaseType": "h2Disk",
                 "prodDatabaseType": "mysql",
-                "searchEngine": "no",
+                "searchEngine": false,
                 "useSass": false,
                 "buildTool": "maven",
                 "frontendBuilder": "grunt",
@@ -50,7 +189,16 @@ module.exports = {
                 "rememberMeKey": "2bb60a80889aa6e6767e9ccd8714982681152aa5",
                 "testFrameworks": [
                     "gatling"
-                ]
+                ],
+                "jhipsterVersion": "4.3.0",
+                "serverPort": "8080",
+                "messageBroker": false,
+                "serviceDiscoveryType": false,
+                "clientFramework": "angular1",
+                "clientPackageManager": "yarn",
+                "applicationType": "monolith",
+                "jhiPrefix": "jhi",
+                "languages": []
             }
         }
     },
