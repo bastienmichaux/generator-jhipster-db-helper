db43a20f45084ab4c818cb761337361239eb43d4
Bastien Michaux <bastienmichaux@gmail.com>
2017-05-16 10:17:06 +0200

    test polyfill

--- test/test-app.js
+++ test/test-app.js
@@ -6,36 +6,62 @@ const fse = require('fs-extra');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
+const dbh = require('../generators/dbh.js');
 const DBH_CONSTANTS = require('../generators/dbh-constants');
 const DBH_TEST_CONSTANTS = require('../generators/dbh-test-constants');
 
 const Generator = require('../generators/app/index.js');
 const GeneratorBase = require('../node_modules/generator-jhipster/generators/generator-base.js');
+const jhipsterModuleSubgenerator = require('../node_modules/generator-jhipster/generators/modules/index.js');
 
 const deps = [
     [helpers.createDummyGenerator(), 'jhipster:modules']
 ];
 
 // replace 'this.log' for testing purposes
-GeneratorBase.prototype.log = msg => console.log(msg);
+// GeneratorBase.prototype.log = msg => console.log(msg);
 
 describe('Post app hook', function () {
     describe('_getPolyfill', function () {
-        beforeEach(function () {
-            return helpers.run(path.join(__dirname + '/../generators/app/index.js'))
-            .inTmpDir(function (dir) {
-                return fse.copySync(path.join(__dirname + '/templates/default/usingMaven'), dir);
-            })
-            .withGenerators(deps)
-            .then(onFulfilled => (onFulfilled), onRejected => console.error(onRejected));
+        it('returns a valid polyfill', function () {
+            const f = path.join(__dirname, 'templates/default/usingMaven/.yo-rc.json');
+            assert.file(f);
+
+            return Generator.prototype._getPolyfill(f)
+            .then(
+                onFulfilled => {
+                    assert(dbh.isNotEmptyString(onFulfilled.baseName));
+                    assert(dbh.isNotEmptyString(onFulfilled.packageName));
+                    assert(dbh.isNotEmptyString(onFulfilled.angularAppName) || onFulfilled.angularAppName === null);
+                    assert(dbh.isNotEmptyString(onFulfilled.clientFramework));
+                    assert(dbh.isNotEmptyString(onFulfilled.clientPackageManager));
+                    assert(dbh.isNotEmptyString(onFulfilled.buildTool) && dbh.isValidBuildTool(onFulfilled.buildTool));
+                    assert(typeof onFulfilled.replaceContent === 'function');
+                    assert(typeof onFulfilled.registerModule === 'function');
+                    assert(typeof onFulfilled.updateEntityConfig === 'function');
+                },
+                onRejected => {
+                    return onRejected;
+                }
+            );
         });
-        // @todo : have a template config file for testing, file must have : Angular app name, clientFramework, packageManager, etc.
-        it('gives the expected polyfill');
     });
-    describe('_replaceNamingStrategies (using Gradle)', function () {
-        it('replaces the naming strategies');
-    });
-    describe('_replaceNamingStrategies (using Maven)', function () {
-        it('replaces the naming strategies');
+    describe('_replaceNamingStrategies', function () {
+        it('works as expected', function () {
+            assert.file(path.join(__dirname, 'templates/default/usingMaven/.yo-rc.json'));
+            assert.file(path.join(__dirname, 'templates/default/usingMaven/pom.xml'));
+            assert.file(path.join(__dirname, 'templates/default/usingMaven/src/test/resources/config/application.yml'));
+            assert.file(path.join(__dirname, 'templates/default/usingMaven/src/main/resources/config/application.yml'));
+            assert.file(path.join(__dirname, 'templates/default/usingGradle/.yo-rc.json'));
+            assert.file(path.join(__dirname, 'templates/default/usingGradle/src/test/resources/config/application.yml'));
+            assert.file(path.join(__dirname, 'templates/default/usingGradle/src/main/resources/config/application.yml'));
+
+            assert.fileContent(path.join(__dirname, 'templates/default/usingMaven/pom.xml'), DBH_CONSTANTS.physicalNamingStrategyOld);
+            assert.fileContent(path.join(__dirname, 'templates/default/usingMaven/src/test/resources/config/application.yml'), DBH_CONSTANTS.physicalNamingStrategyOld);
+            assert.fileContent(path.join(__dirname, 'templates/default/usingMaven/src/main/resources/config/application.yml'), DBH_CONSTANTS.physicalNamingStrategyOld);
+
+            assert.fileContent(path.join(__dirname, 'templates/default/usingGradle/src/test/resources/config/application.yml'), DBH_CONSTANTS.physicalNamingStrategyOld);
+            assert.fileContent(path.join(__dirname, 'templates/default/usingGradle/src/main/resources/config/application.yml'), DBH_CONSTANTS.physicalNamingStrategyOld);
+        });
     });
 });
