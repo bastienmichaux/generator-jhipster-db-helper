a3f5b213483a44be590a32585012d17e789024b9
Adrien Horgnies <aHorgnies@altissia.com>
2017-05-23 16:44:14 +0200

    add one-to-many and many-to-many test cases

--- travis/scripts/02-generate-entities.sh
+++ travis/scripts/02-generate-entities.sh
@@ -3,12 +3,20 @@ set -ev
 # Generate several sets of entities and launch tests after each cases
 
 cd minimalist-app
-cp -t .jhipster "$JHIPSTER_SAMPLES/.jhipster/NakedTable.json" "$JHIPSTER_SAMPLES/.jhipster/DressedTable.json" "$JHIPSTER_SAMPLES/.jhipster/OTO_Slave.json" "$JHIPSTER_SAMPLES/.jhipster/OTO_Owner.json"
+cp -t .jhipster "$JHIPSTER_SAMPLES/.jhipster/NakedTable.json"
+cp -t .jhipster "$JHIPSTER_SAMPLES/.jhipster/DressedTable.json"
+cp -t .jhipster "$JHIPSTER_SAMPLES/.jhipster/OTO_Slave.json" "$JHIPSTER_SAMPLES/.jhipster/OTO_Owner.json"
+cp -t .jhipster "$JHIPSTER_SAMPLES/.jhipster/OTM_One.json" "$JHIPSTER_SAMPLES/.jhipster/OTM_Many.json"
+cp -t .jhipster "$JHIPSTER_SAMPLES/.jhipster/MTM_Slave.json" "$JHIPSTER_SAMPLES/.jhipster/MTM_Owner.json"
 
 yo jhipster:entity NakedTable --force
 yo jhipster:entity DressedTable --force
 yo jhipster:entity OTO_Slave --force
 yo jhipster:entity OTO_Owner --force
+yo jhipster:entity MTM_Slave --force
+yo jhipster:entity MTM_Owner --force
+yo jhipster:entity OTM_One --force
+yo jhipster:entity OTM_Many --force
 
 mvn test
 
