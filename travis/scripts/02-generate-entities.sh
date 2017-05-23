dcf5f14fb6993a747e37a969990e8f71276b2660
Adrien Horgnies <aHorgnies@altissia.com>
2017-05-23 11:18:50 +0200

    generate entities from mocks

--- travis/scripts/02-generate-entities.sh
+++ travis/scripts/02-generate-entities.sh
@@ -5,17 +5,16 @@ set -ev
 cd minimalist-app
 cp -t .jhipster "$JHIPSTER_SAMPLES/.jhipster/NakedTable.json" "$JHIPSTER_SAMPLES/.jhipster/DressedTable.json" "$JHIPSTER_SAMPLES/.jhipster/OTO_Slave.json" "$JHIPSTER_SAMPLES/.jhipster/OTO_Owner.json"
 
-# 2 entities without fields, OTO, jhipster values
-# 2 entities without fields, OTO, stress values
+yo jhipster:entity NakedTable --force
+yo jhipster:entity DressedTable --force
+yo jhipster:entity OTO_Slave --force
+yo jhipster:entity OTO_Owner --force
 
 # 2 entities without fields, OTM, jhipster values
 # 2 entities without fields, OTM, stress values
 
 # 2 entities without fields, MTM, jhipster values
 # 2 entities without fields, MTM, stress values
-
-# 2 entities with fields, OTO, jhipster values
-# 2 entities with fields, OTO, stress values
 
 # 2 entities with fields, OTM, jhipster values
 # 2 entities with fields, OTM, stress values
