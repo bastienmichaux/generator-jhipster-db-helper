21c8e26850d00b8b1fd26784dc6c5d6de42258b3
Adrien Horgnies <aHorgnies@altissia.com>
2017-06-07 09:35:07 +0200

    add match to previous version value (before 4.2.0, not including)

--- generators/fix-entity/index.js
+++ generators/fix-entity/index.js
@@ -180,8 +180,8 @@ module.exports = generator.extend({
             updateKey(`"fieldName": "${columnItem.fieldName}"`, 'dbhColumnName', oldValue, newValue);
 
             // We search either for our value or JHipster value, so it works even if user didn't accept JHipster overwrite while regenerating
-            jhipsterFunc.replaceContent(files.ORM, `@Column\\(name = "(${columnItem.fieldNameAsDatabaseColumn}|${oldValue})`, `@Column(name = "${newValue}`, true);
-            jhipsterFunc.replaceContent(files.liquibaseEntity, `\\<column name="(${columnItem.fieldNameAsDatabaseColumn}|${oldValue})`, `<column name="${newValue}`, true);
+            jhipsterFunc.replaceContent(files.ORM, `@Column\\(name = "(${columnItem.fieldNameAsDatabaseColumn}|${columnItem.fieldNameUnderscored}|${oldValue})`, `@Column(name = "${newValue}`, true);
+            jhipsterFunc.replaceContent(files.liquibaseEntity, `\\<column name="(${columnItem.fieldNameAsDatabaseColumn}|${columnItem.fieldNameUnderscored}|${oldValue})`, `<column name="${newValue}`, true);
         });
 
         // Add/Change/Keep dbhRelationshipId
