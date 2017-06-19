433a114caad0fb9b8375435e6a837361bc760009
Adrien Horgnies <aHorgnies@altissia.com>
2017-06-19 11:40:19 +0200

    add and generate rename-id travis case

--- travis/scripts/02-generate-entities.sh
+++ travis/scripts/02-generate-entities.sh
@@ -11,6 +11,7 @@ $gen 001-table-and-fields
 $gen 002-table-and-relationships
 $gen 003-non-standard-relationships
 $gen 004-generate-twice
+$gen 005-rename-id
 
 # test if generated application works
 mvn test
