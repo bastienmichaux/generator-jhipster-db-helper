8cead4a2d939e24a763f59e7a49ba32e4850a6d0
Trần Quang Tiến <tientq@dkt.com.vn>
2018-01-17 15:51:23 +0700

    add relationship column name

--- generators/fix-entity/prompts.js
+++ generators/fix-entity/prompts.js
@@ -4,7 +4,8 @@ const dbh = require('../dbh.js');
 module.exports = {
     askForTableName,
     askForIdName,
-    askForColumnsName
+    askForColumnsName,
+    askForRelationshipsId
 };
 
 /**
@@ -113,6 +114,74 @@ function askForColumnName(done) {
 
         if (this.field !== undefined) {
             askForColumnName.call(this, done);
+        } else {
+            done();
+        }
+    });
+}
+
+/** For each relationship of entity, ask for actual column name */
+function askForRelationshipsId() {
+    // Don't ask relationship id if there aren't any relationship
+    // Or option --force
+    if (this.relationships === undefined || this.relationships.length === 0 || this.force) {
+        return;
+    }
+
+    // work only on owner relationship
+    this.relationshipsPile = this.relationships.filter((relationshipItem) => {
+        // We don't need to do anything about relationships which don't add any constraint.
+        return !(relationshipItem.relationshipType === 'one-to-many' ||
+            (relationshipItem.relationshipType === 'one-to-one' && !relationshipItem.ownerSide) ||
+            (relationshipItem.relationshipType === 'many-to-many' && !relationshipItem.ownerSide));
+    });
+
+    if (this.relationshipsPile.length === 0) {
+        return;
+    }
+
+    this.log(chalk.green(`Asking column names for ${this.relationshipsPile.length} relationship(s)`));
+    const done = this.async();
+    
+    this.relationship = this.relationshipsPile.pop();
+    askForRelationshipId.call(this, done);
+}
+
+/**
+ * Use ${this.relationship} which is set either by askForRelationshipsId or previous recursive call
+ *
+ * Ask the column name for the relationship of an entity
+ * This function use ${this.relationshipsPile}, at each call it will pop an item from it and ask its question about it.
+ * Then it will associate the answer with this item and push it to ${this.relationshipsInput}.
+ * So at the end of the recursion, ${this.relationshipsPile} will be empty and this.relationshipsInput full with what was in the former.
+ */
+function askForRelationshipId(done) {
+    const validateColumnName = dbh.validateColumnName;
+    const defaultAnswer = this.relationship.dbhRelationshipId || `${this.relationship.relationshipName}_id`;
+
+    const prompts = [
+        {
+            type: 'input',
+            name: 'dbhRelationshipId',
+            validate: ((input) => {
+                const prodDatabaseType = this.jhipsterAppConfig.prodDatabaseType;
+                return validateColumnName(input, prodDatabaseType);
+            }),
+            message: `What column name do you want for the relationship "${this.relationship.relationshipName}" ?`,
+            default: defaultAnswer
+        }
+    ];
+
+    this.prompt(prompts).then((props) => {
+        this.relationship.relationshipIdInput = props.dbhRelationshipId;
+
+        // push just processed item
+        this.relationshipsInput.push(this.relationship);
+        // pop item for next recursion
+        this.relationship = this.relationshipsPile.pop();
+
+        if (this.relationship !== undefined) {
+            askForRelationshipId.call(this, done);
         } else {
             done();
         }
